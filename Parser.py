a1fd89b9d78e7e20e81f188d42f91bc7edf0bc8d
Paul Traylor <paul@wegame.com>
2011-03-02 22:27:22 -0800

    Cleanup for sample client

--- Parser.py
+++ Parser.py
@@ -1,4 +1,4 @@
-from optparse import OptionParser
+from optparse import OptionParser,OptionGroup
 import pydefaults
 
 class ServerParser(OptionParser):
@@ -22,3 +22,61 @@ class ServerParser(OptionParser):
 	def parse_args(self, args=None, values=None):
 		values, args = OptionParser.parse_args(self, args, values)
 		return values, args
+
+class ClientParser(OptionParser):
+	def __init__(self,domain='com.github.kfdm.gntp'):
+		OptionParser.__init__(self)
+		self.settings = pydefaults.database(domain)
+		
+		#Network
+		group = OptionGroup(self,'Network')
+		group.add_option("-H","--host",help="Specify a hostname to which to send a remote notification. [%default]",
+						dest="host",default=self.settings['host'])
+		group.add_option("--port",help="port to listen on",
+						dest="port",type="int",default=self.settings['port'])
+		group.add_option("-P","--password",help="Network password",
+						dest='password',default=self.settings['password'])
+		self.add_option_group(group)
+		
+		#Required (Needs Defaults)
+		group = OptionGroup(self,'Growl')
+		group.add_option("-n","--name",help="Set the name of the application [%default]",
+						dest="app",default='ReGrowl Client')
+		group.add_option("-N","--notification",help="Set the notification name [%default]",
+						dest="name",default='ReGrowl Message')
+		group.add_option("-t","--title",help="Set the title of the notification [Default :%default]",
+						dest="title",default=None)
+		group.add_option("-m","--message",help="Sets the message instead of using stdin",
+						dest="message",default=None)
+		self.add_option_group(group)
+		
+		#Optional (Does not require Default)
+		group = OptionGroup(self,'Extra')
+		group.add_option("-d","--debug",help="Print raw growl packets",
+						dest='debug',action="store_true",default=False)
+		group.add_option("-s","--sticky",help="Make the notification sticky [%default]",
+						dest='sticky',action="store_true",default=False)
+		group.add_option("-p","--priority",help="-2 to 2 [%default]",
+						dest="priority",type="int",default=0)
+		group.add_option("--image",help="Icon for notification (Only supports URL currently)",
+						dest="icon",default=None)
+		self.add_option_group(group)
+	def parse_args(self, args=None, values=None):
+		values, args = OptionParser.parse_args(self, args, values)
+		
+		if values.message is None:
+			print 'Enter a message followed by Ctrl-D'
+			try: message = sys.stdin.read()
+			except KeyboardInterrupt: exit()
+		else:
+			message = values.message
+		
+		if values.title is None:
+			values.title = ' '.join(args)
+		
+		# If we still have an empty title, use the 
+		# first bit of the message as the title
+		if values.title == '':
+			values.title = message[:20]
+		
+		return values, message
--- client.py
+++ client.py
@@ -2,86 +2,13 @@
 
 import sys
 from gntp.notifier import GrowlNotifier
-from config import Config
-from optparse import OptionParser
 
-class ClientConfig(Config):
-	_defaults = {
-		'gntp':{
-			'host':'localhost',
-			'port':23053,
-			'password':'',
-		},
-		'client':{
-			'appname':'pygntp',
-			'name':'Notification Name',
-			'priority':0,
-			'sticky':False,
-			'debug':False,
-			'icon':'',
-		},
-	}
-	_booleans = ['client.debug','client.sticky']
-	_ints = ['gntp.port','client.priority']
-	_editor = True
 
-class ClientParser(OptionParser):
-	def __init__(self,file):
-		OptionParser.__init__(self)
-		self._config = ClientConfig(file)
-		
-		#Network
-		self.add_option("-H","--host",help="Specify a hostname to which to send a remote notification. [%default]",
-						dest="host",default=self._config['gntp.host'])
-		self.add_option("--port",help="port to listen on",
-						dest="port",type="int",default=self._config['gntp.port'])
-		self.add_option("-P","--password",help="Network password",
-					dest='password',default=self._config['server.password'])
-		
-		#Required (Needs Defaults)
-		self.add_option("-n","--name",help="Set the name of the application [%default]",
-						dest="app",default=self._config['client.appname'])
-		self.add_option("-N","--notification",help="Set the notification name [%default]",
-						dest="name",default=self._config['client.name'])
-		self.add_option("-t","--title",help="Set the title of the notification [Default :%default]",
-						dest="title",default=None)
-		self.add_option("-m","--message",help="Sets the message instead of using stdin",
-						dest="message",default=None)
-		
-		#Optional (Does not require Default)
-		self.add_option("-d","--debug",help="Print raw growl packets",
-						dest='debug',action="store_true",default=self._config['client.debug'])
-		self.add_option("-s","--sticky",help="Make the notification sticky [%default]",
-						dest='sticky',action="store_true",default=self._config['client.sticky'])
-		self.add_option("-p","--priority",help="-2 to 2 [%default]",
-						dest="priority",type="int",default=self._config['client.priority'])
-		self.add_option("--image",help="Icon for notification (Only supports URL currently)",
-						dest="icon",default=self._config['client.icon'])
-		self.add_option("-e","--edit",help="Open config in $EDITOR",
-					dest='edit',action="store_true",default=False)
-	def parse_args(self, args=None, values=None):
-		values, args = OptionParser.parse_args(self, args, values)
-		if values.edit: exit(self._config.editor())
-		
-		if values.message is None:
-			print 'Enter a message followed by Ctrl-D'
-			try: message = sys.stdin.read()
-			except KeyboardInterrupt: exit()
-		else:
-			message = values.message
-		
-		if values.title is None:
-			values.title = ' '.join(args)
-		
-		# If we still have an empty title, use the 
-		# first bit of the message as the title
-		if values.title == '':
-			values.title = message[:20]
-		
-		return values, message
 
 if __name__ == "__main__":
-	(options,message) = ClientParser('~/.gntp').parse_args()
+	from Parser import ClientParser
+	
+	(options,message) = ClientParser().parse_args()
 	
 	growl = GrowlNotifier(
 		applicationName = options.app,
