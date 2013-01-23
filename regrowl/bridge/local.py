19d21ad66e653f5b225f62618e5a2cdad6bed90d
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-01-22 22:15:38 -0800

    Refactor common code into regrowl.regrowler

--- regrowl/bridge/local.py
+++ regrowl/bridge/local.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
-import urllib2
 import Growl
 import logging
+
+from regrowl.regrowler import ReGrowler
 
 
 logger = logging.getLogger(__name__)
@@ -11,64 +12,25 @@ logger = logging.getLogger(__name__)
 __all__ = ['LocalNotifier']
 
 
-class LocalNotifier(object):
-    def __init__(self, packet):
-        logger.info('Decoding Packet %s', packet.info['messagetype'])
+class LocalNotifier(ReGrowler):
+    valid = ['REGISTER', 'NOTIFY']
 
-        if packet.info['messagetype'] not in ['NOTIFY', 'REGISTER']:
-            logger.warning('LocalNotifier cannot decode %s', packet.info['messagetype'])
-            return
-
-        # Every packet has an application name
-        self.applicationName = packet.headers['Application-Name']
-        logger.info('Application Name: %s', self.applicationName)
-
-        # Pull out the notification type(s)
-        if packet.info['messagetype'] == 'NOTIFY':
-            self.notifications = [packet.headers['Notification-Name']]
-        elif packet.info['messagetype'] == 'REGISTER':
-            self.notifications = []
-            for name in packet.notifications:
-                self.notifications.append(name['Notification-Name'])
-        logger.info('Notification Name: %s', self.notifications)
-
-        if packet.headers.get('Notification-Icon'):
-            resource = packet.headers['Notification-Icon']
-            self.icon = self.get_resource(packet, resource)
-        else:
-            self.icon = None
-
-        logger.debug('%s', packet.headers)
-
-        self.growl = Growl.GrowlNotifier(
+    def instance(self, packet):
+        return Growl.GrowlNotifier(
             applicationName=self.applicationName,
             notifications=self.notifications,
             defaultNotifications=self.notifications
         )
 
-        if packet.info['messagetype'] == 'REGISTER':
-            self.growl.register()
-        elif packet.info['messagetype'] == 'NOTIFY':
-            self.growl.notify(
-                noteType=packet.headers['Notification-Name'],
-                title=packet.headers['Notification-Title'],
-                description=packet.headers['Notification-Name'],
-                icon=self.icon
-            )
+    def register(self, packet):
+        logger.info('Sending register to local machine')
+        self.growler.register()
 
-    def get_resource(self, packet, resource):
-        try:
-            if resource.startswith('x-growl-resource://'):
-                logger.info('Getting inline resource')
-                resource = resource.split('://')
-                return packet.get(resource[1])['Data']
-            elif resource.startswith('http'):
-                logger.info('Getting url resource: %s', resource)
-                resource = resource.replace(' ', '%20')
-                icon = urllib2.urlopen(resource, None, 5)
-                return icon.read()
-            else:
-                return None
-        except Exception, e:
-            print e
-            return None
+    def notify(self, packet):
+        logger.info('Sending notification to local machine')
+        self.growler.notify(
+            noteType=packet.headers['Notification-Name'],
+            title=packet.headers['Notification-Title'],
+            description=packet.headers['Notification-Name'],
+            icon=self.icon
+        )
---
+++ regrowl/regrowler.py
@@ -0,0 +1,67 @@
+import logging
+import urllib2
+
+logger = logging.getLogger(__name__)
+
+
+class ReGrowler(object):
+    def __init__(self, packet):
+        logger.info('Decoding Packet %s', packet.info['messagetype'])
+
+        if packet.info['messagetype'] not in self.valid:
+            logger.warning('%s cannot decode %s',
+                self, packet.info['messagetype'])
+            return
+
+        # Every packet has an application name
+        self.applicationName = packet.headers['Application-Name']
+        logger.info('Application Name: %s', self.applicationName)
+
+        # Pull out the notification type(s)
+        if packet.info['messagetype'] == 'NOTIFY':
+            self.notifications = [packet.headers['Notification-Name']]
+        elif packet.info['messagetype'] == 'REGISTER':
+            self.notifications = []
+            for name in packet.notifications:
+                self.notifications.append(name['Notification-Name'])
+        logger.info('Notification Name: %s', self.notifications)
+
+        if packet.headers.get('Notification-Icon'):
+            resource = packet.headers['Notification-Icon']
+            self.icon = self.get_resource(packet, resource)
+        else:
+            self.icon = None
+
+        logger.debug('%s', packet.headers)
+
+        self.growler = self.instance(packet)
+        {
+            'REGISTER': self.register,
+            'NOTIFY': self.notify,
+        }.get(packet.info['messagetype'])(packet)
+
+    def get_resource(self, packet, resource):
+        try:
+            if resource.startswith('x-growl-resource://'):
+                logger.info('Getting inline resource')
+                resource = resource.split('://')
+                return packet.get(resource[1])['Data']
+            elif resource.startswith('http'):
+                logger.info('Getting url resource: %s', resource)
+                resource = resource.replace(' ', '%20')
+                icon = urllib2.urlopen(resource, None, 5)
+                return icon.read()
+            else:
+                return None
+        except Exception, e:
+            print e
+            return None
+
+    def instance(self, packet):
+        raise NotImplementedError()
+
+    def register(self, packet):
+        raise NotImplementedError()
+
+    def notify(self, packet):
+        raise NotImplementedError()
