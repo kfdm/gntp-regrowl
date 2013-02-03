95e354814c5c9dc17cbc3c06d2abae26bd50c875
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-02-03 13:40:39 -0800

    Stub for subscribe support

---
+++ regrowl/bridge/subscribe.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from regrowl.regrowler import ReGrowler
+
+
+class LocalNotifier(ReGrowler):
+    valid = ['SUBSCRIBE']
+
+    def instance(self, packet):
+        return None
+
+    def subscribe(self, packet):
+        print '='*80
+        print self.srcaddr
+        print self.srcport
+        print packet.info
+        print '='*80
+        for key in packet.headers:
+            print key, packet.headers[key]
