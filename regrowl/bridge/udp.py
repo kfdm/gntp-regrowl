947f816158a8b4247e4e9ad1a49435b5ccac23b8
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-02-03 13:40:00 -0800

    Initial stab at UDP support

---
+++ regrowl/bridge/udp.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+
+import logging
+from socket import AF_INET, SOCK_DGRAM, socket
+
+from regrowl.regrowler import ReGrowler
+import regrowl.extras.netgrowl as netgrowl
+
+
+logger = logging.getLogger(__name__)
+
+
+__all__ = ['LocalNotifier']
+
+
+class LocalNotifier(ReGrowler):
+    valid = ['REGISTER', 'NOTIFY']
+
+    def _send(self, payload):
+        addr = ("localhost", netgrowl.GROWL_UDP_PORT)
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.sendto(payload, addr)
+
+    def instance(self, packet):
+        return None
+
+    def register(self, packet):
+        p = netgrowl.GrowlRegistrationPacket(
+            application=packet.headers.get('Application-Name')
+            )
+
+        for notification in packet.notifications:
+            p.addNotification(notification.get('Notification-Name'))
+
+        logger.info('Sending UDP register')
+        self._send(p.payload())
+
+    def notify(self, packet):
+        p = netgrowl.GrowlNotificationPacket(
+            application=packet.headers.get('Application-Name'),
+            notification=packet.headers.get('Notification-Name'),
+            title=packet.headers['Notification-Title'],
+            description=packet.headers['Notification-Name'],
+            )
+
+        logger.info('Sending UDP notification')
+        self._send(p.payload())
